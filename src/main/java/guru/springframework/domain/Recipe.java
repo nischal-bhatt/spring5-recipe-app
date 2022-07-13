da88caa81a6092ec16ba4c46e658924a5ca6a160
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-07-13 10:46:01 +0800

    committing first ever commits of stuff for recipe assignmnet

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -32,7 +32,26 @@ public class Recipe {
      @Lob
      private Byte[] image;
 
-     @OneToOne(cascade= CascadeType.ALL)
+     @Enumerated(value = EnumType.STRING)
+     private Difficulty difficulty;
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    @OneToOne(cascade= CascadeType.ALL)
      private Notes notes;
 
     public String getDescription() {
